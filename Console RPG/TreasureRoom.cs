c09cbc1ab5868eeaa92ef4a5ff592372c741c70c
Day <1717222@mukilteo.wednet.edu>
2024-03-07 13:20:56 -0800

    ending

---
+++ Console RPG/TreasureRoom.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_RPG
+{
+    class TreasureRoom : LocationFeature
+    {
+        public List<Item> nonEquipmentLoot;
+        public List<Equipment> equipmentLoot;
+
+        public TreasureRoom(bool isResolved, List<Item> nonEquipmentLoot, List<Equipment> equipmentLoot) : base(isResolved)
+        {
+            this.nonEquipmentLoot = nonEquipmentLoot;
+            this.equipmentLoot = equipmentLoot;
+        }
+
+        public override void Resolve(List<Player> players)
+        {
+            Console.WriteLine("Seems like there is some loot here.\n");
+            Console.WriteLine("Here I'll put them into your inventory.\n");
+            foreach (var item in nonEquipmentLoot)
+            {
+                Console.WriteLine($"You got a/an {item.name}!");
+                Player.inventory.Add(item);
+            }
+            foreach (var equipment in equipmentLoot)
+            {
+                Console.WriteLine($"You got a/an {equipment.name}!");
+                Player.equipmentInventory.Add(equipment);
+            }
+            isResolved = true;
+        }
+    }
+}
